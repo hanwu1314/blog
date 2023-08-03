84505b46a1db1072669d727e272e84a7fcec2705
hanwu <hanwu1314@outlook.com>
2023-08-03 11:52:41 +0800

    :style: style: 修改打包路径为根目录

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/blog/",
+  base: "/",
 
   lang: "zh-CN",
   title: "寒舞知识宝库",
